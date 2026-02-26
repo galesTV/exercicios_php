51a0699ab1837f825a359d07b635afa0d72a8217
galesTV <gaelleiteguzman@gmail.com>
2026-02-25 23:13:05 -0300

    refactor(helpers): make resumirTexto dynamic by adding parameters and adjusting index implementation

--- blog/helpers.php
+++ blog/helpers.php
@@ -4,8 +4,8 @@ function saudacao() {
     return "Olá, seja bem-vindo ao nosso blog!";
 }
 
-function resumirTexto(){
-    return "Este é um resumo do texto completo.";
+function resumirTexto($texto, $limite, $continue = '...'){
+    return $texto;
 }
 
 ?>
--- blog/index.php
+++ blog/index.php
@@ -4,7 +4,9 @@
 require_once 'sistema/configuracao.php'; // Inclui o arquivo de configuração do sistema
 include_once 'helpers.php'; // Inclui o arquivo de funções auxiliares
 
+$texto = "Este é um exemplo de texto longo que será resumido.";
+
 echo saudacao(); // Chama a função de saudação definida em helpers.php
 echo '<hr>'; // Exibe uma linha horizontal para separar a saudação do resumo
-echo resumirTexto(); // Chama a função de resumo de texto definida em helpers.php
+echo resumirTexto($texto, 50); // Chama a função de resumo de texto definida em helpers.php
 ?>
