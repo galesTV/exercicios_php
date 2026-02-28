cd336edccd7ba9524f9feed86a50a5a3bd26d3b7
galesTV <gaelleiteguzman@gmail.com>
2026-02-28 19:51:19 -0300

    feat(helpers): add conditional greeting based on time using if/elseif/else

--- blog/helpers.php
+++ blog/helpers.php
@@ -1,7 +1,18 @@
 <?php
 
 function saudacao() {
-    return "Olá, seja bem-vindo ao nosso blog!";
+    $hora = 19; // Simulando a hora atual (19 horas, ou seja, 7 da noite)
+    $saudacao = "";
+
+    if($hora >= 0 && $hora < 12) {
+        $saudacao = "Bom dia!";
+    } else if ($hora >= 12 && $hora < 18) {
+        $saudacao = "Boa tarde!";
+    } else {
+        $saudacao = "Boa noite!";
+    }
+
+    return $saudacao;
 }
 
 function resumirTexto(string $texto, int $limite, string $continue = '...'): string{
