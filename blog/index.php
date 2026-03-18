8d32df0bbd25b9a63f71316a2274812b3f54094c
galesTV <gaelleiteguzman@gmail.com>
2026-03-18 00:01:47 -0300

    feat(config): define constants using define() and test access via constant() in index.php

--- blog/index.php
+++ blog/index.php
@@ -3,3 +3,7 @@
 
 require_once 'sistema/configuracao.php'; // Inclui o arquivo de configuração do sistema
 include_once 'helpers.php'; // Inclui o arquivo de funções auxiliares
+
+echo SITE_NOME; // Exibe o nome do site definido na configuração
+echo '<hr>';
+echo constant('SITE_NOME'); // Exibe o nome do site usando a função constant
--- blog/sistema/configuracao.php
+++ blog/sistema/configuracao.php
@@ -1,3 +1,13 @@
 <?php
     date_default_timezone_set('America/Sao_Paulo'); // Define o fuso horário para São Paulo
+
+    // Informações do site
+    define('SITE_NOME', 'galesTV');
+    define('SITE_DESCRICAO', 'Um blog sobre tecnologia e inovação');
+
+    // URLs para produção e desenvolvimento
+    define('URL_PRODUCAO', 'https://gales-tv.vercel.app/');
+    define('URL_DESENVOLVIMENTO', 'http://localhost/blog');
+
+    // const SITE_NOME = 'galesTV'; // Define o nome do site
 ?>
