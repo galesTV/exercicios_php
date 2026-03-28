d6b4dc00c198e186e6042f74fca9d8ef0083dfc8
galesTV <gaelleiteguzman@gmail.com>
2026-03-28 19:39:17 -0300

    feat(nucleo): enhance Mensagem class with encapsulation and rendering method

--- blog/index.php
+++ blog/index.php
@@ -6,6 +6,6 @@ include_once 'helpers.php'; // Inclui o arquivo de funções auxiliares
 include_once 'sistema/nucleo/mensagem.php'; // Inclui o arquivo da classe de mensagens
 
 $msg = new Mensagem(); // Cria uma instância da classe de mensagens
-echo $msg->texto = 'texto de teste'; // Exibe o texto da mensagem
-echo '<br>';
+echo $msg->renderizar(); // Chama o método renderizar para exibir a mensagem
+echo '<hr>';
 echo var_dump($msg); // Exibe a estrutura da variável $msg para depuração
--- blog/sistema/nucleo/mensagem.php
+++ blog/sistema/nucleo/mensagem.php
@@ -1,7 +1,30 @@
 <?php
 
+/**
+ * Classe Mensagem - responsável por renderizar e filtrar mensagens
+ * @author Gael Guzman
+ */
 class Mensagem
 {
-    public $texto; // Propriedade para armazenar o texto da mensagem
-    public $css; // Propriedade para armazenar a classe CSS da mensagem
+    private $texto; // Propriedade para armazenar o texto da mensagem
+    private $css; // Propriedade para armazenar a classe CSS da mensagem
+
+    /**
+     * Renderiza a mensagem
+     * @return string
+     */
+    public function renderizar(): string
+    {
+        return $this->texto = $this->filtrar('<h1>Mensagem de teste</h1>'); // Define o texto da mensagem
+    }
+
+    /**
+     * Filtra a mensagem
+     * @param string $mensagem
+     * @return string
+     */
+    private function filtrar(string $mensagem): string
+    {
+        return filter_var($mensagem, FILTER_DEFAULT); // Filtra a mensagem para remover caracteres indesejados
+    }
 }
